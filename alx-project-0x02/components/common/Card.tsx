3dd26517ff42f340af697d8dcb0b19f73831fd50
KhothatsoS <khothatsosibasa@gmail.com>
2025-11-30 21:14:15 +0200

    Adding and using the Card component

---
+++ alx-project-0x02/components/common/Card.tsx
@@ -0,0 +1,17 @@
+import import React from "react";
+
+interface CardProps {
+    title: string;
+    content: string;
+}
+
+const Card: React.FC<CardProps> = ({ title, content }) => {
+    return (
+    <div className="border rounded-lg shadow-md p-4 bg-white">
+      <h2 className="text-lg font-semibold mb-2">{title}</h2>
+      <p className="text-gray-700">{content}</p>
+    </div>
+  );
+};
+
+export default Card;
--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,12 +1,24 @@
 import React from "react";
+import Card from "../components/common/Card";
 
 const HomePage = () => {
   return (
     <div>
       <h1>Home</h1>
       <p>Welcome to the Home Page.</p>
+
+      <Card
+        title="Latest Updates"
+        content="Check out the latest news, updates, and improvements made this week."
+      />
+
+      <Card
+        title="User Tips"
+        content="Discover useful tips that help you get the most out of your experience."
+      />
     </div>
   );
 };
+
 
 export default HomePage;
